6181a078aab955e076a710b623a6dd5becf0fdbe
drkbldwn208 <bderek731@gmail.com>
2026-04-07 18:36:48 -0700

    Hooking up dacs to dummy constant module to debug dac1

---
+++ RedPitayaCavityRelocker.srcs/sources_1/new/axis_constant.v
@@ -0,0 +1,12 @@
+module axis_constant(
+    input wire clk,
+    output wire [31:0] m_axis_tdata,
+    output wire m_axis_tvalid
+    );
+    
+    
+    assign m_axis_tdata = 32'h1B58_1B58;
+    assign m_axis_tvalid = 1'b1;
+    
+    
+endmodule
